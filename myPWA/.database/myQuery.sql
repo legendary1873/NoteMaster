9387d8ae21583147e86f1693452f8ab45ce50bf7
jiayu <jiayu.hu01@gmail.com>
2025-10-28 22:31:30 +1100

    set up everything

---
+++ myPWA/.database/myQuery.sql
@@ -0,0 +1,3 @@
+CREATE TABLE extension(extID INTEGER NOT NULL PRIMARY KEY,name TEXT NOT NULL, hyperlink
+TEXT NOT NULL,about TEXT NOT NULL,image TEXT NOT NULL,language TEXT NOT NULL);
+INSERT INTO extension(extID,name,hyperlink,about,image,language) VALUES (X,"","","","",""); Insert template
